d2a1330cbffa03e9cfd5da6f07d7af2d69daf218
Iria Blanco Cid <iria.blanco-cid@essencedigital.com>
2017-03-24 11:59:14 +0000

    Add Exercise1

---
+++ Exercise1_CharacterInput.py
@@ -0,0 +1,6 @@
+# Instructions for all these exercises are here: http://www.practicepython.org/
+
+name = raw_input("Please, tell me your name ")
+age = int(raw_input("How old are you? "))
+
+print(" Hi " + name + ", you will be 100 years old in " + str(100-age) + "years")
