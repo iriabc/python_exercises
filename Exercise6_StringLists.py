ca6f6495684449e74e1ca34cb1481f2dabc10aca
Iria Blanco Cid <iria.blanco-cid@essencedigital.com>
2017-03-24 15:08:38 +0000

    Adding Exercise6

---
+++ Exercise6_StringLists.py
@@ -0,0 +1,10 @@
+word = raw_input("Can you please give me a string? ")
+
+if word[::1] == word [::-1]:
+    print "Your word is a palindrome"
+else:
+    print "Your word is not a palindrome"
+
+
+
+
